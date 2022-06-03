84563891cf08d92ab5f035479d3b97315f8a63fd
Ivan Dronek <nomi29251@gmail.com>
2022-06-03 16:26:02 +0300

    Task 1-33

--- Web/BeatifulSoap/task 10.py
+++ Web/BeatifulSoap/task 10.py
@@ -2,4 +2,4 @@ import requests
 from bs4 import BeautifulSoup
 r = requests.get(url="https://python.org/")
 soup = BeautifulSoup(r.text, "lxml")
-print(soup.select("a")[0:10])
+print(soup.select("a")[0:10])#
--- Web/BeatifulSoap/task 9.py
+++ Web/BeatifulSoap/task 9.py
@@ -3,4 +3,4 @@ from bs4 import BeautifulSoup
 r = requests.get(url="https://python.org/")
 soup = BeautifulSoup(r.text, "lxml")
 print(soup.findAll("h2"))
-print(soup.select("h2")[0:4])
+print(soup.select("h2")[0:4])#
